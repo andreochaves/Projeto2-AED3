ca0c4ca9378c83d46bb8bc4d4572cee77f57be9f
Marco Antônio Patrocinio <73245980+MarcoPatrocinio@users.noreply.github.com>
2021-12-07 20:18:21 -0300

    Create exercicio3.cs

---
+++ exercicio3.cs
@@ -0,0 +1,2 @@
+#Resposta = Considerado o O(N²/³) é quase equivalente a uma função logaritimica,
+#eu compraria apenas se minha base de dados para rodar essa operação fosse muito grande e compensase a demora para se estabilzar a curva logaritimica
